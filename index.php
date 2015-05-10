7968776997e4d0395cf8be18560b45cb02d848eb
Alex <git@github.com:cheese124>
2015-05-09 22:35:19 -0400

    add sql in for 1-6 and test

--- index.php
+++ index.php
@@ -13,41 +13,39 @@ echo '<a href="index.php?page=R3">Colleges with the largest endowment overall</a
 echo '<a href="index.php?page=R4">Colleges with the largest enrollment of freshman</a><br>';
 echo '<a href="index.php?page=R5">Colleges with the highest revenue from tuition</a><br>';
 echo '<a href="index.php?page=R6">Colleges with the lowest non zero tuition revenue</a><br>';
-
 echo '<a href="index.php?page=R7">The top 10 colleges by region</a><br>';
 
 echo '<br><br>';
 //Page 1
 if($_GET['page']=="R1")
 {
-	$sql = 'select employees.emp_no, employees.first_name, employees.last_name, salaries.salary from
-	 employees left join salaries on employees.emp_no = salaries.emp_no order by salary DESC limit 1;';
+	$sql = 'select institution, percent_women from (select UNITID, institution from hd2013) as lable1 left join (select UNITID, ((total_women/total)*100) as percent_women from ef2013b) as lable2 on lable1.UNITID = lable2.UNITID WHERE NOT percent_women IS NULL group by lable1.institution order by percent_women desc;';
 }
 //Page 2
 if($_GET['page']=="R2")
 {
-        $sql = 'select employees.emp_no, employees.first_name, employees.last_name, salaries.salary from employees left join salaries on employees.emp_no = salaries.emp_no where salaries.from_date between "1981-01-01" and "1985-01-01" order by salary DESC limit 1;';
+        $sql = 'select institution, percent_men from (select UNITID, institution from hd2013) as lable1 left join (select UNITID, ((total_men/total)*100) as percent_men from ef2013b) as lable2 on lable1.UNITID = lable2.UNITID WHERE NOT percent_men IS NULL group by lable1.institution order by percent_men desc;';
 }
 	
 //Page 3
 if($_GET['page']=="R3")
 {
-        $sql = 'select employees.emp_no, employees.first_name, employees.last_name, departments.dept_name, salaries.salary from employees left join salaries on employees.emp_no = salaries.emp_no join titles on employees.emp_no = titles.emp_no join dept_manager on  dept_manager.emp_no = employees.emp_no join departments on departments.dept_no = dept_manager.dept_no where titles.title = "Manager" and salaries.to_date = "9999-01-01" order by salaries.salary DESC limit 1;';
+        $sql = 'select institution, endowment from (select UNITID, institution from hd2013) as lable1 left join (select UNITID, endowment from f1213_f1a) as lable2 on lable1.UNITID = lable2.UNITID WHERE NOT endowment IS NULL and not endowment = 0 group by lable1.institution order by endowment desc;';
 }
 //Page 4
 if($_GET['page']=="R4")
 {
-        $sql = 'select dept_name from departments group by dept_name;';
+        $sql = 'select institution, total_of_men_and_women_enrolled from (select UNITID, institution from hd2013) as lable1 left join (select UNITID, total_of_men_and_women_enrolled from ef2013a) as lable2 on lable1.UNITID = lable2.UNITID WHERE NOT total_of_men_and_women_enrolled IS NULL and not total_of_men_and_women_enrolled = 0 group by lable1.institution order by total_of_men_and_women_enrolled desc;';
 }
 //Page 5
 if($_GET['page']=="R5")
 {
-        $sql = 'select employees.emp_no, employees.first_name, employees.last_name, titles.title from employees left join titles on employees.emp_no = titles.emp_no where titles.title = "Manager" and titles.to_date = "9999-01-01";';
+        $sql = 'select institution, tuition from (select UNITID, institution from hd2013) as lable1 left join (select UNITID, tuition from f1213_f1a) as lable2 on lable1.UNITID = lable2.UNITID WHERE NOT tuition IS NULL and not tuition = 0 group by lable1.institution order by tuition desc;';
 }
 //Page 6
 if($_GET['page']=="R6")
 {
-        $sql = 'select employees.emp_no, employees.first_name, employees.last_name, titles.title, salaries.salary from employees left join salaries on employees.emp_no = salaries.emp_no join titles on employees.emp_no = titles.emp_no where titles.title != "Manager" order by salaries.salary DESC limit 1;';
+        $sql = 'select institution, tuition from (select UNITID, institution from hd2013) as lable1 left join (select UNITID, tuition from f1213_f1a) as lable2 on lable1.UNITID = lable2.UNITID WHERE NOT tuition IS NULL and not tuition = 0 group by lable1.institution order by tuition asc;';
 }
 //Page 7
 if($_GET['page']=="R7")
