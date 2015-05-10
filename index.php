e209d95004d932096843fcfcc2bfc27776c497c1
Alex <git@github.com:cheese124>
2015-05-09 22:42:09 -0400

    remove all unused code

--- index.php
+++ index.php
@@ -50,84 +50,9 @@ if($_GET['page']=="R6")
 //Page 7
 if($_GET['page']=="R7")
 {       
-	 $sql = 'select employees.emp_no, employees.first_name, employees.last_name, salaries.salary from employees left join salaries on employees.emp_no = salaries.emp_no order by salaries.emp_no  ASC limit 1;';
+	 $sql = '';
 }
-//Page 8
-if($_GET['page']=="R8")
-{       
-	 $sql = 'select departments.dept_name, count(departments.dept_name) from dept_emp left join employees on employees.emp_no = dept_emp.emp_no join departments on departments.dept_no = dept_emp.dept_no where to_date = "9999-01-01" group by dept_name;';
-}
-//Page 9
-if($_GET['page']=="R9")
-{       
-	 $sql = 'select departments.dept_name, sum(salaries.salary) from dept_emp left join employees on employees.emp_no = dept_emp.emp_no join departments on departments.dept_no = dept_emp.dept_no join salaries on employees.emp_no = salaries.emp_no where dept_emp.to_date = "9999-01-01" group by dept_name;';
-}
-//Page 10
-if($_GET['page']=="R10")
-{       
-	 $sql = 'select sum(salaries.salary) from employees left join salaries on employees.emp_no = salaries.emp_no join titles on employees.emp_no = titles.emp_no;';
-}
-//Insert a new Employee
-if($_GET['page']=="insert")
-{
-	echo "<form name='Insert Employee' action='insert.php' method='get'>";
-	echo "First Name <input type=text name='firstname' placeholder='First Name'><br>";
-	echo "Last Name <input type=text name='lastname' placeholder='Last Name'><br>";
-	echo "Birth data <input type=text name='birthdate' placeholder='yyyy-mm-dd'><br>";
-	echo "Hire data <input type=text name=hiredate placeholder='yyyy-mm-dd'><br>";
-	echo "<input type='radio' name='gender' value='M'>Male<br>";
-	echo "<input type='radio' name='gender' value='F'>Female<br>";	
-	
-	echo "<input type=submit id='Insert' value='Add Employee'>";
-        echo "</form>";
-}
-//Update an employee
-if($_GET['page']=="update")
-{
-	$sql = "select * from employees order by emp_no desc limit 5;";
-	
-        $num=1;
-        echo "<table border='1' style='width:100%' table-layout: fixed>";
-        foreach($db->query($sql) as $row)
-        {
-        
-                //remove duplicates     
-                $remove=0;
-                foreach($row as $x)
-                {
-                        unset($row[$remove]);
-                        $remove++;
-                }
-			echo "<form name='Update Employee' action='update.php' method='get'>";
-			$emp = ($row['emp_no']);
-                        //Printer each entrey number in the table
-                        echo "<tr>";
-                        echo "<td>";
-                        echo ("Result Number");
-                        echo "</td>";
-                        echo "<td>";
-			echo ($num."   ");
-			echo "<input type=submit id='Update' value='Update'>";
-			echo "<input type='hidden' name='emp' value='$emp'";
-                        echo "</td>";
-                        echo "</tr>";
-			echo "</form>";   
-                foreach ($row as $key => $value)
-                {
-                        echo "<tr>";
-                        echo "<td>";
-                        echo ($key);
-                        echo "</td>";
-                        echo "<td>";
-                        echo ($value);
-                        echo "</td>";
-                        echo "</tr>";
-                }
-                $num++;
-                echo("<tr><td><br></td><td><br></td></tr>");
-	}
-	echo "</table>";
-}
+
 // Sorts the array and prints it
 // This statment detects if the button clicked is a request R
 if (strpos($_GET['page'],"R")===0)
@@ -169,12 +94,5 @@ if (strpos($_GET['page'],"R")===0)
 	}
 	echo "</table>";
 }    
-/*
-$sql = 'SELECT * FROM departments LIMIT 5';
-foreach($db->query($sql) as $row) {
-print_r($row);
-print("<br> /");
-}
-print_r($db);
-*/
+
 ?>
